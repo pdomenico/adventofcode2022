19dee36ef033d6a31bbac67e67bb40f353fe9240
pdomenico <domenicoprocopio.a@gmail.com>
2023-02-18 23:27:31 +0100

    day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -6,7 +6,32 @@ enum Packet {
     Number(u32),
 }
 
-type PacketCouple = (Packet, Packet);
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match compare_packets(self, other) {
+            Some(true) => Ordering::Less,
+            Some(false) => Ordering::Greater,
+            None => Ordering::Equal,
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        match compare_packets(self, other) {
+            Some(_) => false,
+            None => true,
+        }
+    }
+}
+
+impl Eq for Packet {}
 
 fn print_packet(packet: &Packet) {
     match packet {
@@ -93,27 +118,27 @@ fn parse_packet(s: &str) -> Packet {
     Packet::List(elements.into_iter().map(Box::new).collect())
 }
 
-fn compare_packets((p1, p2): &PacketCouple) -> Option<bool> {
+fn compare_packets(p1: &Packet, p2: &Packet) -> Option<bool> {
     match (p1, p2) {
         (Packet::Number(a), Packet::Number(b)) => match a.cmp(&b) {
             Ordering::Less => Some(true),
             Ordering::Greater => Some(false),
             Ordering::Equal => None,
         },
-        (Packet::Number(a), Packet::List(list)) => compare_packets(&(
-            Packet::List(vec![Box::new(Packet::Number(*a))]),
-            Packet::List(list.to_vec()),
-        )),
-        (Packet::List(list), Packet::Number(b)) => compare_packets(&(
-            Packet::List(list.to_vec()),
-            Packet::List(vec![Box::new(Packet::Number(*b))]),
-        )),
+        (Packet::Number(a), Packet::List(list)) => compare_packets(
+            &Packet::List(vec![Box::new(Packet::Number(*a))]),
+            &Packet::List(list.to_vec()),
+        ),
+        (Packet::List(list), Packet::Number(b)) => compare_packets(
+            &Packet::List(list.to_vec()),
+            &Packet::List(vec![Box::new(Packet::Number(*b))]),
+        ),
         (Packet::List(list1), Packet::List(list2)) => {
             let mut res = None;
             let (mut list1_iter, mut list2_iter) = (list1.iter(), list2.iter());
             while let None = res {
                 res = match (list1_iter.next(), list2_iter.next()) {
-                    (Some(a), Some(b)) => compare_packets(&(*a.clone(), *b.clone())),
+                    (Some(a), Some(b)) => compare_packets(a, b),
                     (Some(_), None) => Some(false),
                     (None, Some(_)) => Some(true),
                     (None, None) => break,
@@ -124,7 +149,7 @@ fn compare_packets((p1, p2): &PacketCouple) -> Option<bool> {
     }
 }
 
-fn read_input(filepath: &str) -> Vec<PacketCouple> {
+fn read_input(filepath: &str) -> Vec<(Packet, Packet)> {
     let file = File::open(filepath).unwrap();
     let mut reader = io::BufReader::new(file).lines();
     let mut packets = Vec::new();
@@ -148,8 +173,8 @@ pub fn part1() {
     let result = packets
         .into_iter()
         .enumerate()
-        .filter_map(|(i, couple)| {
-            if compare_packets(&couple).unwrap() {
+        .filter_map(|(i, (p1, p2))| {
+            if compare_packets(&p1, &p2).unwrap() {
                 Some(i + 1)
             } else {
                 None
@@ -158,4 +183,23 @@ pub fn part1() {
         .fold(0, |a, b| a + b);
 
     println!("{}", result);
+}
+
+pub fn part2() {
+    let packets_couples = read_input("inputs/day13.txt");
+    let mut packets = Vec::new();
+
+    for (p1, p2) in packets_couples.into_iter() {
+        packets.push(p1);
+        packets.push(p2);
+    }
+
+    let (div1, div2) = (parse_packet("[2]"), parse_packet("[6]"));
+    packets.push(div1.clone());
+    packets.push(div2.clone());
+
+    packets.sort();
+    let position1 = packets.iter().position(|e| *e == div1).unwrap();
+    let position2 = packets.iter().position(|e| *e == div2).unwrap();
+    println!("{}", (position1 + 1) * (position2 + 1));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use advent_of_code::day13;
 
 fn main() {
-    day13::part1();
+    day13::part2();
 }
